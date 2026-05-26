8f4baad06cc217bda30ab8ece50d63997c310135
gratusrichard <gratusri@gmail.com>
2026-05-26 22:55:33 +0100

    Fix config for Vercel

--- next.config.js
+++ next.config.js
@@ -1,11 +1,2 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: '/mohit',
-  assetPrefix: '/mohit',
-};
-
-export default nextConfig;
+const nextConfig = {};  
+module.exports = nextConfig;  
